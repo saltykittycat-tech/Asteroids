6fffa55be033f397277824aadb60bb3041cc54ab
saltykittycat-tech <saltykittycat@gmail.com>
2025-08-25 11:25:11 -0500

    set framerate

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
