1b3413bcd9f4462f941b846d5bdc03b4bf9899de
saltykittycat-tech <saltykittycat@gmail.com>
2025-08-26 15:31:48 -0500

    made player sprite turn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import *
 from player import *
-pygame.init
+pygame.init ()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
@@ -20,8 +20,8 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
